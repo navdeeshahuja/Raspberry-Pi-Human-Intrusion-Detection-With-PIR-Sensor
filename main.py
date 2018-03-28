412cdb9e65a7d6886d1b39e840a53dbc6e9f76c0
Navdeesh Ahuja <navdeeshahuja@Navdeeshs-MacBook-Air.local>
2018-03-28 11:41:03 +0530

    sam

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import RPi.GPIO as GPIO
 import time
+import json, requests
 
 AUTHORITY_PHONE_NUMBER = 8527250263
-
+sent = False
 def informAuthority():
-	# inform authority here
-	pass
+    if sent:
+        return
+	link = "https://snowbarter.com/sendpranav.php?mob={}&senderId=INTRSN&message=intrusion%20detected".format(AUTHORITY_PHONE_NUMBER)
+    resp = requests.get(url=link, verify=False)
+    sent = True
+    print("responseFromServer -> ", resp.text)
 
 GPIO.setmode(GPIO.BCM)
 
